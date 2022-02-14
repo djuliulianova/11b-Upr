f081fa3207f741e0cd5de06c99bdc7d1e264788f
djuliulianova <Student@DESKTOP-0JD15UO>
2022-02-14 10:29:56 +0200

    add project ListManipulation

---
+++ RemoveFirst/Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int first = nums[0];
+            while(nums.Contains(first))
+            {
+                nums.Remove(first);
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
